489a46913e4316a63d4024dc19acdfe28bf5e3ab
Bruno Pereira <bruno9pereira@gmail.com>
2016-10-26 18:06:14 -0200

    add users routes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(cookieParser());
 
 app.use('/feeds', require('./routes/v1/feeds'));
 app.use('/episodes', require('./routes/v1/episodes'));
+app.use('/users', require('./routes/v1/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
