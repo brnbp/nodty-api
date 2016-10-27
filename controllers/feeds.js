724c46d240a9c28f5a564b350fae7ee36f9e347d
Bruno Pereira <bruno9pereira@gmail.com>
2016-10-27 18:05:11 -0200

    create controller for feeds

---
+++ controllers/feeds.js
@@ -0,0 +1,21 @@
+const defaultResponse = (data, statusCode = 200) => ({data, statusCode})
+const errorResponse = (message = '', statusCode = 400) => ({error: message, statusCode})
+
+function FeedsController(FeedsModel) {
+    this.FeedsModel = FeedsModel
+
+    this.getById = id => defaultResponse(id)
+                //this.FeedsModel.findOne(id)
+
+    this.latests = () => defaultResponse({latests:[]})
+
+    this.top = (count = 24) => defaultResponse({top:[count]})
+
+    this.getByName = (name) => defaultResponse({name})
+
+    this.episodes = (id) => defaultResponse({episodes: [id]})
+
+
+}
+
+module.exports = FeedsController
