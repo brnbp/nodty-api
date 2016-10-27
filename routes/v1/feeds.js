3ab560c151ace8be8012aa14759bc5403920b10e
Bruno Pereira <bruno@lik3.com.br>
2016-10-27 18:05:11 -0200

    create controller for feeds

--- routes/v1/feeds.js
+++ routes/v1/feeds.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const FeedsModel = {}
+const ControllerFeed = require('../../controllers/feeds')
+const FeedsController = new ControllerFeed(FeedsModel)
 
 router.get('/latests', function(req, res, next) {
-    res.send('respond with a resource');
+    res.send(FeedsController.latests());
 });
 
 router.get('/top/:count?', function(req, res, next) {
-    res.send('respond with a resource');
+    res.send(FeedsController.top(req.params.count));
 });
 
 router.get('/name/:name', function(req, res, next) {
-    res.send('respond with a resource');
+    res.send(FeedsController.getByName(req.params.name));
 });
 
 router.get('/:id', function(req, res, next) {
-    res.send('respond with a resource');
+    res.send(FeedsController.getById(req.params.id));
 });
 
 router.get('/:id/episodes', function(req, res, next) {
-    res.send('respond with a resource');
+    res.send(FeedsController.episodes(req.params.id));
 });
 
 module.exports = router;
