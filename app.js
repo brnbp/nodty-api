11c0500857193d4b745076ac845047898e548cf0
Bruno Pereira <bruno@lik3.com.br>
2016-10-27 14:45:58 -0200

    add queues routes

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(cookieParser());
 app.use('/feeds', require('./routes/v1/feeds'));
 app.use('/episodes', require('./routes/v1/episodes'));
 app.use('/users', require('./routes/v1/users'));
+app.use('/queues', require('./routes/v1/queues'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,6 +45,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
